70eff8f251c03ec936bc85cc9cb953a28e162a2d
Tulio Salvatierra <salvacorp@gmail.com>
2024-12-05 15:32:43 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+name = input("what is your name?: ")
+age = int(input("How old are you?: "))
+height = float(input("how tall are you?: "))
+
+print("Hello "+name)
+print("You are "+str(age)+" years old")
+print("You are "+str(height)+ "cm tall ")
