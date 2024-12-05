935e5132da73804f98fc8bf767070c98c6ad290e
Tulio Salvatierra <salvacorp@gmail.com>
2024-12-05 16:33:07 -0600

    added practicing files to python leaning

---
+++ forLoop.py
@@ -0,0 +1,4 @@
+shopping_list = ["apple", "carrot", "banana"]
+
+for i in shopping_list: 
+    print(i)
---
+++ programming-lab-3.py
@@ -0,0 +1,32 @@
+### basic strutures of programming
+
+## 1. Sequential
+
+x = 5
+print(x)
+
+## 2. Conditional
+
+if x > 3: 
+    print("x is greater than 3")
+
+## 3. loops
+days_of_the_week = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+for i in range(5):
+    print(i)
+
+### Problem 2: Write Python for statements to display each element of a string named testStr.
+
+
+testStr = "problem of the programming lab"
+for element in testStr: 
+    print(element)
+
+### 2) Write Python statements to read your name from the keyboard and print the output.
+
+name = input("what is your name?: ")
+print("your name is: ", name)
+
+### 3) Write a Python statement to create a week_days_list structure from ‘Monday’ to ‘Friday’
+
+print(days_of_the_week)
