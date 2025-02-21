405fc06af950201bc49e785bb4f1733051aec198
Tulio Salvatierra <salvacorp@gmail.com>
2025-02-20 18:59:29 -0600

    add pyramid pattern printing script

---
+++ pythonPiramid.py
@@ -0,0 +1,14 @@
+
+#this program will print 2 patterns
+
+#this is the ascending pattern
+print("Pattern A")
+for i in range(1, 11):
+    print("+" * i)
+
+#this is the pspace between them
+print()
+#this is the descending pattern
+print("Pattern B")
+for i in range(10, 0, -1):
+    print("+" * i)
